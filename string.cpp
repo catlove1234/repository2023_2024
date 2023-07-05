a8c6bb185acb48de2dd04bc1dd1349666334df87
catlove1234 <138655225+catlove1234@users.noreply.github.com>
2023-07-05 10:10:21 +0200

    Add files via upload

---
+++ string.cpp
@@ -0,0 +1,24 @@
+/*
+wprowadzenie do funkcji "string"
+wersja 1.0
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  char znak;
+  string napis;//string s³u¿y do przechowywania ca³ych napisów, tylko tekst(tablica znaków)
+  int tablica_intigerow[20];
+
+  napis = "Ula"; //pojedyñcze litery dajemy w ', a ca³e napisy w "
+  napis += " jest fajna";// do napisu masz dodaæ "jest fajna"
+  cout << napis;
+
+
+  //znak = 'g';
+  //cout << znak;
+    return 0;
+}
